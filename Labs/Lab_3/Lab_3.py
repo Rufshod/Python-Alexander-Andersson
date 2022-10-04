e45ad66a93f34da15f7f6f687ee0a3a2c0c4dcb5
Alexander Andersson <alexander@11a.se>
2022-10-04 13:53:09 +0200

    Started to sketch up lab3

---
+++ Labs/Lab_3/Lab_3.py
@@ -0,0 +1,68 @@
+#TODO
+
+#Shape class
+# + __init__(self, x_pos ,y_pos)
+#+ __repr__()
+#+ __str__()
+
+#+ __lt__(self, other) <
+#+ __gt__(self, other) >
+#+ __le__(self, other) <=
+#+ __ge__(self, other) >=
+#+ translation(x, y) -> Shape
+
+#- in_object(self, point) -> bool:
+
+# x_pos: int = 0
+# y_pos: int = 0
+
+# area = None
+# omkrets = None
+
+
+#Circle class
+
+# - radie: float = 1
+# x_pos: float = 0
+# y_pos: float = 0
+
+# Radie Getter
+# @property 
+#def radie(self):
+
+
+# Radie Setter 
+
+
+# def is_unit_circle(self) -> bool:
+    # if unit is circle
+        # return True
+        # else:
+        # return false
+        # EXAMPLE
+        
+
+
+# def calc_area(self) -> float:
+    # radie ** Pi.
+
+# def calc_circumfurence(self) -> float:
+    #calculating circum
+    #return circum_answer.
+
+
+#Rectangle class
+
+# @property getter
+
+# @width.setter
+
+# @height.getter
+
+# def is_square(self) -> bool:
+    # h*w/2 = x**2
+    
+# def calc_area(self) > float:
+    #h**2 + w**2
+
+# CALC CIRCUM?????
