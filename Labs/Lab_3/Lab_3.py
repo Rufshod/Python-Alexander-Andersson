3648b05e6a6ec3b683123de01a5ac672e3ef0a0c
Alexander Andersson <alexander@11a.se>
2022-10-05 13:52:13 +0200

    Started Lab 3. trying to fix x-pos not defined err

--- Labs/Lab_3/Lab_3.py
+++ Labs/Lab_3/Lab_3.py
@@ -1,6 +1,49 @@
 #TODO
-
+from __future__ import annotations
+import matplotlib.pyplot as plt
+import numpy as np
+from math import sqrt
+#TODO 
 #Shape class
+class Shape:
+    """The Super class named Shape"""
+    def __init__(self, x_pos: int | float, y_pos: int | float) -> None:
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+
+
+# x Position getter
+    @property
+    def get_x_pos(self) -> int | float:
+        """Getter for x_pos"""
+        return self._x_pos
+
+# x Position setter
+
+    @x_pos.setter
+    def x_pos(self, value: int | float) -> int | float:
+        if not isinstance(value, (int, float)): # checks if the type in x_pos is int or float
+            raise TypeError(f"x Position must be an int or float, not {type(value).__name__}")
+        # if not 0 <= value <= 1000: TODO, SETTING THIS FOR LATER IF NEEDED.
+        self._x_pos = value
+
+# y Position getter
+    @property
+    def get_y_pos(self) -> int | float:
+        """Getter for y_pos"""
+        return self._y_pos
+
+# y Position setter
+
+    @y_pos.setter
+    def y_pos(self, value: int | float) -> int | float:
+        if not isinstance(value, (int, float)): # checks if the type in y_pos is int or float
+            raise TypeError(f"y Position must be an int or float, not {type(value).__name__}")
+        # if not 0 <= value <= 1000: TODO, SETTING THIS FOR LATER IF NEEDED.
+        self._y_pos = value
+
+x = Shape(1,2)
+
 # + __init__(self, x_pos ,y_pos)
 #+ __repr__()
 #+ __str__()
