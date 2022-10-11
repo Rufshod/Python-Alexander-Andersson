01b46afa54ef1e8d270ad88233813488fa94310e
Rufshod <alexander@11a.se>
2022-10-11 20:00:23 +0200

    Need to fix point inside circle method

--- Labs/Lab_3/Lab_3.py
+++ Labs/Lab_3/Lab_3.py
@@ -1,6 +1,7 @@
 # TODO
 from __future__ import annotations
 from turtle import circle
+
 import matplotlib.pyplot as plt
 import numpy as np
 import math
@@ -24,6 +25,12 @@ class Shape:
         """X position for the Shape"""
         return self._x_pos
 
+    # y Position getter
+    @property
+    def y_pos(self) -> int | float:
+        """Y position for the Shape"""
+        return self._y_pos
+
     # x Position setter
 
     @x_pos.setter
@@ -35,13 +42,7 @@ class Shape:
                 f"x Position must be an int or float, not {type(value).__name__}"
             )
 
-        self._x_pos = value # Sets the x value
-
-    # y Position getter
-    @property
-    def y_pos(self) -> int | float:
-        """Y position for the Shape"""
-        return self._y_pos
+        self._x_pos = value  # Sets the x value
 
     # y Position setter
 
@@ -54,8 +55,7 @@ class Shape:
                 f"y Position must be an int or float, not {type(value).__name__}"
             )
 
-        self._y_pos = value # Sets the y value
-
+        self._y_pos = value  # Sets the y value
 
     # -------------------- Overloading Operators, Shape Class  -------------------- #
 
@@ -90,18 +90,18 @@ class Shape:
         x_value: float | int,
         y_value: float | int,
     ):  # This will add the values to self.x or self.y
-        """Translation method is used in order to change the values of the x, y position for """
+        """Translation method is used in order to change the values of the x, y position for"""
         self.x_pos += x_value
         self.y_pos += y_value
 
     # + __repr__()
     def __repr__(self) -> str:
-        """When print function is called, returns the x position and y position of the shape"""
-        return f"The shape's position is x = {self.x_pos}, y = {self.y_pos}"
+        """Representation of the shapes positional values"""
+        return f"Shape's position x = {self.x_pos}, y = {self.y_pos}"
 
     # + __str__()
     def __str__(self) -> str:
-        """STR function for Superclass shape"""
+        """When print function is called, returns the x position and y position of the shape"""
         return f"The shape's position is x = {self.x_pos}, y = {self.y_pos}"
 
 
@@ -129,10 +129,13 @@ class Circle(Shape):
 
     @c_radius.setter
     def c_radius(self, value: (float | int)):  # Setter
-        if not isinstance(value, (float, int)):  #Checks to see if the value is float or int
+        if not isinstance(
+            value, (float, int)
+        ):  # Checks to see if the value is float or int
             raise TypeError(f"Radius must be float or int, not {type(value).__name__}")
-        else:
-            self._c_radius = value
+        if value <= 0:
+            raise ValueError(f"The value of the radius must be positive, not {value}")
+        self._c_radius = value
 
     @property
     def area(self) -> (int | float):
@@ -156,19 +159,20 @@ class Circle(Shape):
     @property
     def point_inside_circle(self, x: (float | int), y: (float | int)) -> bool:
         """Method to see if the point is inside the circle shape"""
-        if math.hypot(x - self.x_pos, y - self.y_pos):
+        if not isinstance((x, y), (float, int)):
+            raise TypeError("X or Y must be int or float")
+        if math.hypot(x - self.x_pos, y - self.y_pos ) <= self.c_radius:  # uses the hypot module from math in order to calculate the euclidian distance
             return True
         else:
             return False
 
-
-    ### PLACEHOLDER CODE!!!!!!!
+    # -------------------- Representation and String __Methods__ Circle -------------------- #
     def __repr__(self) -> str:
         """String representation of class Circle"""
         return f"Circle = x = {self.x_pos}, y = {self.y_pos}, radius = {self.c_radius}."
 
     def __str__(self) -> str:
-        """Describes self as a string for printing"""
+        """Override string function"""
         return f"The Circle's position is x: {self.x_pos}, y: {self.y_pos}. The radius is: {self.c_radius}, Area: {self.area}, Circumference: {self.circumference}."
 
 
@@ -201,10 +205,24 @@ class Rectangle(Shape):
 
     @r_height.setter
     def r_height(self, value: float | int):
+        if not isinstance(
+            value, (float, int)
+        ):  # Checks to see if the value is float or int
+            raise TypeError(f"Height must be float or int, not {type(value).__name__}")
+        if value <= 0:
+            raise ValueError(f"The value of height must be positive, not {value}")
+
         self._r_height = value
 
     @r_width.setter
     def r_width(self, value: float | int):
+        if not isinstance(
+            value, (float, int)
+        ):  # Checks to see if the value is float or int
+            raise TypeError(f"Height must be float or int, not {type(value).__name__}")
+        if value <= 0:
+            raise ValueError(f"The value of height must be positive, not {value}")
+
         self._r_width = value
 
     # -------------------- Area Method - Rectangle -------------------- #
@@ -228,6 +246,7 @@ class Rectangle(Shape):
 
     @property
     def perimiter(self) -> float | int:
+        """Method used to see the perimiter of the rectangle"""
         return (self.r_height + self.r_width) * 2
 
     # -------------------- Is Point Inside - Rectangle -------------------- #
@@ -235,5 +254,22 @@ class Rectangle(Shape):
     def point_inside_rectangle(self) -> bool:
         """Method to see if the point is inside the rectangle shape"""
 
+    # -------------------- Representation and String __Methods__ Rectangle -------------------- #
+    def __repr__(self) -> str:
+        """String representation of class Rectangle"""
+        return f"Rectangle = x = {self.x_pos}, y = {self.y_pos}, height = {self.r_height}, width = {self.r_width}."
+
+    def __str__(self) -> str:
+        """Override string function"""
+        return f"The Rectangle's position is x: {self.x_pos}, y: {self.y_pos}. The height and width is: {self.r_height}, {self.r_width}. Area: {self.area}, Perimiter: {self.perimiter}."
 
 
+r = Rectangle(1,1,1,1)
+print(r)
+print(r.is_square)
+
+c = Circle(1,1,1)
+print(c)
+print(c.is_unit_circle)
+
+c.point_inside_circle(0,0)
